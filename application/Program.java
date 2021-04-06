f8beb98923c6337acaccc8bf452eb119ca584a49
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-04-05 21:32:28 -0300

    Add some changes in my code

--- application/Program.java
+++ application/Program.java
@@ -1,15 +1,24 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.enums.WorkerLevel;
+import entities_app.Department;
+import entities_app.HourContract;
+import entities_app.Worker;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		//Author: João Vítor Souza Pioner | Date: 03/04/21 8:56
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("DD/MM/YYYY");
 		
 		System.out.println("Enter department's name: ");
 		String departmentName = sc.nextLine();
@@ -20,6 +29,30 @@ public class Program {
 		String workerLevel = sc.nextLine();
 		System.out.print("Base salary: ");
 		double baseSalary = sc.nextDouble();
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+		
+		System.out.print("How many contracts to this worker? ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Enter contract #" +i+"data:");
+			System.out.print("Date DD/MM/YYYY: ");
+			Date contractDate = sdf.parse(sc.next());
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			System.out.print("Duration (hours): ");
+			int hours = sc.nextInt();
+			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+			worker.addContract(contract);
+		}
+		
+		System.out.println();
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = sc.next();
+		int month = Integer.parseInt( monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: " + worker.getNam());
+		System.out.println("Department: " + worker.getDepartment());
 	}
 
 }
